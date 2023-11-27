049faeec3d0e1110151bae565fced4000350363e
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2023-11-27 16:45:24 +0300

    Update shownetworkobjects.py

--- shownetworkobjects.py
+++ shownetworkobjects.py
@@ -1,7 +1,7 @@
 import argparse
 from cpapi import APIClient, APIClientArgs
-# just show all host and network type objects
-def show_network_objects(server, username, password):
+# show selected objects by type from management or any object type
+def show_objects(server, username, password, obj_type):
     # Setup client
     client_args = APIClientArgs(server=server)
     with APIClient(client_args) as client:
@@ -10,21 +10,21 @@ def show_network_objects(server, username, password):
         if login_res.success is False:
             print("Login failed:", login_res.error_message)
             exit(1)
-        print("Network Hosts and Objects:")
+        print(f"{obj_type.capitalize()} Objects:")
         # Initialize variables for pagination and storing objects
         all_objects = []
         offset = 0
         limit = 200
-        total = 1  # Dummy value to start the loop
-        # Fetch objects of type 'CpmiHostCkp' and 'network_objects'
+        total = 1 
+        # Fetch objects of the specified type or all objects if obj_type is "any"
         while offset < total:
             show_objects_res = client.api_call("show-objects", {"offset": offset, "limit": limit})
             if show_objects_res.success:
                 objects_batch = show_objects_res.data.get("objects", [])
                 total = show_objects_res.data.get("total", 0)
-                # Filter for objects of type 'host' and 'network'
+                # Filter for objects of the specified type or show all objects
                 for obj in objects_batch:
-                    if obj.get("type") in ["host", "network"]:
+                    if obj_type == "any" or obj.get("type") == obj_type:
                         print(f"UID: {obj['uid']}, Name: {obj['name']}, Type: {obj['type']}")
                         all_objects.append({
                             "uid": obj["uid"],
@@ -36,12 +36,12 @@ def show_network_objects(server, username, password):
             else:
                 print("Failed to retrieve objects:", show_objects_res.error_message)
                 break
-        # Logout
         client.api_call("logout")
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Retrieve and print network hosts and objects.")
+    parser = argparse.ArgumentParser(description="Retrieve and print network objects of a specified type or all objects.")
     parser.add_argument("server", help="Server address")
     parser.add_argument("username", help="Username")
     parser.add_argument("password", help="Password")
+    parser.add_argument("obj_type", help="Type of objects to retrieve (e.g., host, network, any)")
     args = parser.parse_args()
-    show_network_objects(args.server, args.username, args.password)
+    show_objects(args.server, args.username, args.password, args.obj_type)
